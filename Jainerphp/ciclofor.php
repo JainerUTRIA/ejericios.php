897fbbcb264154d2c530a3e2758067ed3aef0712
JainerUTRIA <jainerfigueroa39@gmail.com>
2022-07-18 15:05:54 -0500

    Desarrollo: trabajo php

---
+++ Jainerphp/ciclofor.php
@@ -0,0 +1,41 @@
+<?php
+
+    //ciclo for
+    /*
+    echo "Hola Jainer";
+    echo "Hola Jainer";
+    echo "Hola Jainer";
+    echo "Hola Jainer";
+    echo "Hola Jainer";
+    echo "Hola Jainer";
+    echo "Hola Jainer";
+    echo "Hola Jainer";
+    echo "Hola Jainer";
+    echo "Hola Jainer";
+    */
+
+    // hola jainer
+    for ($i=0; $i<=10 ; $i++) {
+        echo "Hola Jainer <br/>";
+    }
+
+    // numeros del 1 al 20
+    for ($numero=1; $numero<=20; $numero++) {
+        echo $numero . "<br/>";
+    }
+
+    // numeros del 1 al 30 de 2 en 2
+    for ($numero=1; $numero<=20; $numero=$numero+2) {
+        echo $numero . "<br/>";
+    }
+
+
+echo "-----------------------";
+
+    // numeros del 1 al 30 de 2 en 2
+    for ($numero=1; $numero<=20; $numero+=2) {
+        echo $numero . "<br/>";
+    }
+
+
+?>
---
+++ Jainerphp/variables.php
@@ -0,0 +1,21 @@
+<?php
+    // Esto es un comentario...
+
+    // mi primera impresion
+    echo "Hola Jainer";
+    echo "<br/>";
+
+    $nombre = "Jainer";
+    $edad = 16;
+    $apellido = "Utria";
+
+
+    // concatenacion
+    $imprimir = $nombre ." ". $apellido . " " . $edad;
+    echo $imprimir;
+    
+
+
+
+
+?>
